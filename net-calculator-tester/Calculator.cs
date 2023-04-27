eb7cce66298bf963906df2256d46a797f339d53a
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-27 09:21:39 +0200

    create methods

--- net-calculator-tester/Calculator.cs
+++ net-calculator-tester/Calculator.cs
@@ -8,6 +8,30 @@ namespace net_calculator_tester
 {
     public class Calculator
     {
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
 
+        public float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Divide(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return num1 / num2;
+        }
+
+        public float Multiply(float num1, float num2)
+        {
+            if (num2 == 0) throw new Exception("Una numero diviso per 0 non produce alcun risultato");
+            return num1 * num2;
+        }
     }
 }
